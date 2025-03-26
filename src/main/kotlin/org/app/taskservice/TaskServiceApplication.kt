d8b32ce96d9d734b7e8f619b9ecf2c4bac6064df
Valerii Mikhailov <mikhailov972@gmail.com>
2025-03-26 10:19:29 +0300

    Draft

--- src/main/kotlin/org/app/taskservice/TaskServiceApplication.kt
+++ src/main/kotlin/org/app/taskservice/TaskServiceApplication.kt
@@ -2,7 +2,6 @@ package org.app.taskservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
 class TaskServiceApplication
